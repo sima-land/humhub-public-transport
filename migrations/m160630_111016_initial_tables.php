13966f8ffdb420a561e237c46edc49559ba035dc
seedykh1 <alex.sedykh00@gmail.com>
2016-06-30 18:15:35 +0500

    Add files via upload

--- migrations/m160630_111016_initial_tables.php
+++ migrations/m160630_111016_initial_tables.php
@@ -9,63 +9,63 @@ use yii\db\Migration;
 
 class m160630_111016_initial_tables extends Migration
 {
-    public function up()
+    public function safeUp()
     {
         $this->createTable('node', [
             'id' => $this->primaryKey(),
-            'name' => $this->text(),
-            'coordinates' => $this->text()
+            'name' => $this->char(40),
+            'coor_x'=>$this->float(10),
+            'coor_y'=>$this->float(10),
         ]);
         $this->createTable('route', [
             'id' => $this->primaryKey(),
-            'direction'=>$this->boolean(),
-            'title'=>$this->text(),
-            'def_bus_id'=>$this->integer(),
+            'direction_id'=>$this->integer(),
+            'title'=>$this->char(50),
         ]);
-        $this->createTable('schedule', array(
-            'date_time' => $this->date(),
+        $this->createTable('direction', [
+            'id' => $this->primaryKey(),
+            'direction'=>$this->char(10),
+        ]);
+        $this->createTable('schedule', [
+            'date_at' => $this->date(),
             'route_id'=>$this->integer(),
             'start_time'=>$this->time(),
             'comment'=>$this->text(),
-        ));
-        $this->createTable('connector', [
+        ]);
+        $this->createTable('route_node', [
             'route_id' => $this->integer(),
             'node_id' => $this->integer(),
-            'interval'=>$this->text(),
+            'node_interval'=>$this->text(),
         ]);
 
-        $this->addForeignKey('r_id','schedule','route_id','route','id',null,null);
-        $this->addForeignKey('r2_id','connector','route_id','route','id',null,null);
-        $this->addForeignKey('n_id','connector','node_id','node','id',null,null);
-        $this->addForeignKey('def_b_id','route','def_bus_id','connector','route_id',null,null);
-    }
+        $this->addPrimaryKey('pk_route_node_route_id_node_id','route_node','route_id,node_id');
 
-    public function down()
-    {
+        $this->addForeignKey('fk_schedule_route_id','schedule','route_id','route','id',null,null);
+        $this->addForeignKey('fk_route_node_route_id','route_node','route_id','route','id',null,null);
+        $this->addForeignKey('fk_route_node_node_id','route_node','node_id','node','id',null,null);
+        $this->addForeignKey('fk_route_direction_id','route','direction_id','direction','id',null,null);
 
-        $this->dropForeignKey('def_b_id','route');
-        $this->dropIndex('def_b_id','route');
-
-        $this->dropForeignKey('r2_id','connector');
-        $this->dropForeignKey('n_id','connector');
-
-        $this->dropIndex('r2_id','connector');
-        $this->dropIndex('n_id','connector');
-
-        $this->dropTable('schedule');
-        $this->dropTable('connector');
-        $this->dropTable('route');
-        $this->dropTable('node');
-    }
-
-    /*
-    // Use safeUp/safeDown to run migration code within a transaction
-    public function safeUp()
-    {
+        $this->insert('direction', [
+            'direction'=> 'на работу',
+        ]);
+        $this->insert('direction', [
+            'direction'=> 'с работы',
+        ]);
     }
 
     public function safeDown()
     {
+        $this->dropForeignKey('fk_schedule_route_id','schedule');
+        $this->dropForeignKey('fk_route_direction_id','route');
+        $this->dropForeignKey('fk_route_node_route_id','route_node');
+        $this->dropForeignKey('fk_route_node_node_id','route_node');
+
+
+        $this->dropTable('schedule');
+        $this->dropTable('route_node');
+        $this->dropTable('route');
+        $this->dropTable('node');
+        $this->dropTable('direction');
+
     }
-    */
 }
